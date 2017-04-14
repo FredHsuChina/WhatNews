7e35d9defcfa18dd840dee88e29f1a6660f8443d
FredHsu <FredHsu0822@hotmail.com>
2017-04-14 14:17:36 +0800

    Initial Commit

---
+++ WhatNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhatNews
+//
+//  Created by 许文锋 on 2017/4/14.
+//  Copyright © 2017年 fred. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhatNews/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WhatNews
+//
+//  Created by 许文锋 on 2017/4/14.
+//  Copyright © 2017年 fred. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
